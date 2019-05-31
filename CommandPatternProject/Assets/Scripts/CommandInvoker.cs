e5896f4a98afbee854688f0174460fa6d9d2f9cc
Batora07 <batora.ushiromiya.07@gmail.com>
2019-05-31 23:09:41 +0200

    Add first basic implementation of the project (cube spawner)

---
+++ CommandPatternProject/Assets/Scripts/CommandInvoker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker : MonoBehaviour
+{
+	private static Queue<ICommand> commandBuffer;
+
+	private void Awake()
+	{
+		commandBuffer = new Queue<ICommand>();
+	}
+
+	public static void AddCommand(ICommand command)
+	{
+		commandBuffer.Enqueue(command);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(commandBuffer.Count > 0)
+		{
+			commandBuffer.Dequeue().Execute();
+		}
+    }
+}
